243b5e11878ed0246415b0cbf69719141bf6a981
cpmatrix@live.com <cpmatrix@live.com>
2018-07-24 19:16:22 +0200

    ComMasterDetail

---
+++ CumejaBeach/Impostazioni.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CumejaBeach
+{
+    public partial class Impostazioni : ContentPage
+    {
+        public Impostazioni()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CumejaBeach/MasterPageItem.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CumejaBeach
+{
+    public partial class MasterPageItem : MasterDetailPage
+    {
+        public MasterPageItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
