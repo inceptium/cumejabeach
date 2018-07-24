69f36f041cd5907366d402e057fa519ac1847a5d
cpmatrix@live.com <cpmatrix@live.com>
2018-07-24 18:09:14 +0200

    Con Tastierino

---
+++ CumejaBeach/Tastierino.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CumejaBeach
+{
+    public partial class Tastierino : ContentPage
+    {
+        public Tastierino()
+        {
+            InitializeComponent();
+        }
+    }
+}
