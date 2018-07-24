48a896b4d6709ee82467af2c0806325e1b4dda72
antoniocaputo <cpmatrix@gmail.com>
2018-07-24 14:49:09 +0200

    con BoxGrid

--- CumejaBeach/MainPage.xaml.cs
+++ CumejaBeach/MainPage.xaml.cs
@@ -13,7 +13,26 @@ namespace CumejaBeach
         {
             InitializeComponent();
             Indicator1.IsRunning = true;
+            disegnaOmbrelloni();
+
+        }
+        private void disegnaOmbrelloni()
+        {
+            var Omb1 = new Label { Text = "Top Left" };
+            var Omb2 = new Label { Text = "Top Right" };
+            var Omb3 = new Label { Text = "Bottom Left" };
+            var Omb4 = new Label { Text = "Bottom Right" };
+
+            GrigliaOmbrelloni.Children.Add(Omb1,0,0);
+            GrigliaOmbrelloni.Children.Add(Omb2,0,1);
+            GrigliaOmbrelloni.Children.Add(Omb3,1,0);
+            GrigliaOmbrelloni.Children.Add(Omb4,1,1);
+
+            //var img1=new Image {}
+
 
         }
     }
+
+
 }
