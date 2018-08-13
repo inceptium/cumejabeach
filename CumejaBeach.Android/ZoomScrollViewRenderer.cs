594f98464af9b5890ae3cb9bce651b250121c3c4
antoniocaputo <cpmatrix@gmail.com>
2018-08-13 15:01:19 +0200

    AttivatoZoomAndroid

---
+++ CumejaBeach.Android/ZoomScrollViewRenderer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using static Android.Views.ScaleGestureDetector;
+using Android.Views.Animations;
+using Xamarin.Forms;
+using ZoomableApp.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.ScrollView), typeof(ZoomScrollViewRenderer))]
+namespace ZoomableApp.Droid.Renderers
+{
+
+    public class ZoomScrollViewRenderer : ScrollViewRenderer, IOnScaleGestureListener
+    {
+        private float mScale = 1f;
+        private ScaleGestureDetector mScaleDetector;
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+
+            base.OnElementChanged(e);
+            mScaleDetector = new ScaleGestureDetector(Context, this);
+
+        }
+
+
+        public override bool DispatchTouchEvent(MotionEvent e)
+        {
+            base.DispatchTouchEvent(e);
+            return mScaleDetector.OnTouchEvent(e);
+        }
+
+        public bool OnScale(ScaleGestureDetector detector)
+        {
+            float scale = 1 - detector.ScaleFactor;
+
+            float prevScale = mScale;
+            mScale += scale;
+
+            if (mScale < 0.1f) // Minimum scale condition:
+                mScale = 0.1f;
+            
+            if (mScale > 2f) // Maximum scale condition:
+                mScale = 2f;
+            ScaleAnimation scaleAnimation = new ScaleAnimation(2f / prevScale, 2f / mScale, 2f / prevScale, 2f / mScale, detector.FocusX, detector.FocusY);
+            scaleAnimation.Duration = 0;
+            scaleAnimation.FillAfter = true;
+            StartAnimation(scaleAnimation);
+            return true;
+        }
+
+        public bool OnScaleBegin(ScaleGestureDetector detector)
+        {
+            return true;
+        }
+
+        public void OnScaleEnd(ScaleGestureDetector detector)
+        {
+
+        }
+    }
+}
