5a4a35dedce20c4c524e184a83dfcf105bb7970e
Antonio <cpmatrix@mac.com>
2019-07-01 16:21:48 +0200

    CumejaRingMainPage

---
+++ CumejaRing/CumejaRing/MainMenu.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CumejaRing
+{
+    public partial class MainMenu : ContentPage
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+            var tapRecognizer = new TapGestureRecognizer();
+            tapRecognizer.Tapped += TapRecognizer_TappedAsync;
+            stack_chiama_cameriere.GestureRecognizers.Add(tapRecognizer);
+        }
+
+        private async void TapRecognizer_TappedAsync(object sender, EventArgs e)
+        {
+            var page = new MainPage();
+           
+            await Navigation.PushAsync(page);
+        }
+    }
+}
