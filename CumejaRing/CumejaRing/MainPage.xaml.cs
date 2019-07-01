e19545dcc456cf81ef50e80bb32a5b7ceda35758
Antonio <cpmatrix@mac.com>
2019-07-01 15:10:49 +0200

    ConCumejaRing

---
+++ CumejaRing/CumejaRing/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.InputKit.Shared.Controls;
+using Xamarin.Forms;
+
+namespace CumejaRing
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public string CurrentLocationSelection { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            stack_status_monitor.IsVisible = true;
+        }
+
+        void radio_button_clicked(object sender, System.EventArgs e)
+        {
+            CurrentLocationSelection=(sender as RadioButton).Text;
+            stack_NumeroPosto.IsVisible = true;
+            bt_chiama.IsVisible = true;
+
+        }
+    }
+}
